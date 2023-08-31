59a8117c1d296df1eb092bb49c222fc7c7cb2896
tanggd7 <tanggd7@qq.com>
2023-08-31 09:00:59 +0800

    feat: 优化代码，并添加必要注释

--- src/practice/extractFrame/index.js
+++ src/practice/extractFrame/index.js
@@ -4,11 +4,16 @@ ipt.onchange = async function (e) {
   const file = e.target.files[0]
 
   for (let i = 0; i < 10; i++) {
+    // 获取每帧的图片信息，{ url, blob }
     const frame = await extractFrame(file, i)
     createPreview(frame)
   }
 }
 
+/**
+ * 将帧图片加入到 html 中
+ * @param {Object} frame 帧图片
+ */
 function createPreview(frame) {
   const img = document.createElement("img")
   img.src = frame.url
@@ -16,6 +21,12 @@ function createPreview(frame) {
   document.body.appendChild(img)
 }
 
+/**
+ * 生成一个 video ，在等待准备完成后，将 video 时间点的状态绘到 canvas 上
+ * @param {File} videoFile 视频文件
+ * @param {Number} time 帧时间节点
+ * @returns Promise<{url, blob}>
+ */
 function extractFrame(videoFile, time = 0) {
   return new Promise(function (resolve) {
     const video = document.createElement("video")
@@ -30,6 +41,11 @@ function extractFrame(videoFile, time = 0) {
   })
 }
 
+/**
+ * 将 video 某一时间点绘到 canvas 上，并导出 url 和 blob
+ * @param {Video} video
+ * @returns Promise<{url, blob}>
+ */
 function draw(video) {
   return new Promise(function (resolve) {
     const canvas = document.createElement("canvas")
